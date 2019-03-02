28df2e6f9c0d31abd35d77a7897ba7e9554a442d
Mike Stenzler <mike@nexuscafe.com>
2019-03-02 10:34:58 -0500

    Initial Commit

---
+++ FOREXDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FOREXDemo
+//
+//  Created by Michael Stenzler on 3/2/19.
+//  Copyright © 2019 Michael Stenzler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
