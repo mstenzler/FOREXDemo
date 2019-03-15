1d5bdd2d870d23b1bcac5278d480002290698b6c
Mike Stenzler <mike@nexuscafe.com>
2019-03-15 00:31:45 -0400

    Working

---
+++ FOREXDemo/FavoriteCurrencyPairTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteCurrencyPairTableViewCell.swift
+//  FOREXDemo
+//
+//  Created by Michael Stenzler on 3/15/19.
+//  Copyright © 2019 Michael Stenzler. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCurrencyPairTableViewCell: UITableViewCell {
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
---
+++ FOREXDemo/Items.swift
@@ -0,0 +1,14 @@
+//
+//  Items.swift
+//  FOREXDemo
+//
+//  Created by Michael Stenzler on 3/15/19.
+//  Copyright © 2019 Michael Stenzler. All rights reserved.
+//
+
+import Foundation
+
+class Items {
+    static let sharedInstance = Items()
+    var favorites = [String]()
+}
